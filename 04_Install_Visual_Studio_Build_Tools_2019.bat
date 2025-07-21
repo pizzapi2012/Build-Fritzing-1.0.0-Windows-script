93778467336271b7fb5a87be93c911d74b4bc652
A Crazy Pencil <157081760+pizzapi2012@users.noreply.github.com>
2025-07-21 12:07:19 -0500

    Use Line Format

--- 04_Install_Visual_Studio_Build_Tools_2019.bat
+++ 04_Install_Visual_Studio_Build_Tools_2019.bat
@@ -1,12 +1,12 @@
 @echo off
 rem 04_Install_Visual_Studio_Build_Tools_2019.bat
-rem http://www.neko.ne.jp/~freewing/software/build_fritzing_100_windows/
+rem http:^//www.neko.ne.jp/~freewing/software/build_fritzing_100_windows/
 rem Copyright (c) 2023 FREE WING,Y.Sakamoto
 echo %0
 timeout /T 10 /NOBREAK
 cd /d \00_fritzing
 
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools" exit
+if exist "C:^\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools" exit
 
 
 @echo off
@@ -28,7 +28,24 @@ pause
 set VSDIR=%cd%
 vs_BuildTools_2019.exe --config %VSDIR%\VSBT_VS2019.vsconfig ^
   --norestart --passive --wait ^
-  --add Microsoft.VisualStudio.Component.Roslyn.Compiler;Microsoft.Component.MSBuild;Microsoft.VisualStudio.Component.CoreBuildTools;Microsoft.VisualStudio.Workload.MSBuildTools;Microsoft.VisualStudio.Component.Windows10SDK;Microsoft.VisualStudio.Component.VC.CoreBuildTools;Microsoft.VisualStudio.Component.VC.Tools.x86.x64;Microsoft.VisualStudio.Component.VC.Redist.14.Latest;Microsoft.VisualStudio.Component.Windows10SDK.19041;Microsoft.VisualStudio.Component.VC.CMake.Project;Microsoft.VisualStudio.Component.TestTools.BuildTools;Microsoft.VisualStudio.Component.VC.ASAN;Microsoft.VisualStudio.Component.TextTemplating;Microsoft.VisualStudio.Component.VC.CoreIde;Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core;Microsoft.VisualStudio.Workload.VCTools;includeRecommended;includeOptional
+  --add ^
+    Microsoft.VisualStudio.Component.Roslyn.Compiler;^
+    Microsoft.Component.MSBuild;^Microsoft.VisualStudio.Component.CoreBuildTools;^
+    Microsoft.VisualStudio.Workload.MSBuildTools;^
+    Microsoft.VisualStudio.Component.Windows10SDK;^
+    Microsoft.VisualStudio.Component.VC.CoreBuildTools;^
+    Microsoft.VisualStudio.Component.VC.Tools.x86.x64;^
+    Microsoft.VisualStudio.Component.VC.Redist.14.Latest;^
+    Microsoft.VisualStudio.Component.Windows10SDK.19041;^
+    Microsoft.VisualStudio.Component.VC.CMake.Project;^
+    Microsoft.VisualStudio.Component.TestTools.BuildTools;^
+    Microsoft.VisualStudio.Component.VC.ASAN;^
+    Microsoft.VisualStudio.Component.TextTemplating;^
+    Microsoft.VisualStudio.Component.VC.CoreIde;^
+    Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core;^
+    Microsoft.VisualStudio.Workload.VCTools;^
+    includeRecommended;^
+    includeOptional
 
 
 exit
